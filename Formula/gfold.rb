b5d720f95142d5ebb25bbbc2d715223f646880a5
Nick Gerace <nickagerace@gmail.com>
2021-10-29 19:02:30 -0400

    Update gfold URL to new tar gz location

--- Formula/gfold.rb
+++ Formula/gfold.rb
@@ -1,7 +1,7 @@
 class Gfold < Formula
   desc "CLI tool to help keep track of your Git repositories, written in Rust"
   homepage "https://nickgerace.dev"
-  url "https://github.com/nickgerace/gfold/archive/2.0.0.tar.gz"
+  url "https://github.com/nickgerace/gfold/archive/refs/tags/2.0.0.tar.gz"
   sha256 "54c04df0c4e91903d810c09d41d6c8606219cda00e6c3b5ed4e9821bb4a72c08"
   license "Apache-2.0"
 
