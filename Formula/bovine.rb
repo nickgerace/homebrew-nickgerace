18b6a57c40146d6d8d9e683b0275bf587ecefb83
Nick Gerace <nickagerace@gmail.com>
2021-12-02 16:27:33 -0500

    Update gfold to 2.0.2 and bovine to 1.0.3

--- Formula/bovine.rb
+++ Formula/bovine.rb
@@ -1,8 +1,8 @@
 class Bovine < Formula
   desc "Manager for single node Rancher clusters"
   homepage "https://nickgerace.dev"
-  url "https://github.com/nickgerace/bovine/archive/refs/tags/1.0.2.tar.gz"
-  sha256 "37e6ccd81a99f810eb9f4cf11e1c3e3096975274a36e8aaafd2461af0e0d4c29"
+  url "https://github.com/nickgerace/bovine/archive/refs/tags/1.0.3.tar.gz"
+  sha256 "96124261df565751e10656a6e27ce4f5e78e7e623152929a22096663238030e8"
   license "Apache-2.0"
 
   depends_on "rust" => :build
--- Formula/gfold.rb
+++ Formula/gfold.rb
@@ -1,8 +1,8 @@
 class Gfold < Formula
   desc "CLI tool to help keep track of your Git repositories, written in Rust"
   homepage "https://nickgerace.dev"
-  url "https://github.com/nickgerace/gfold/archive/refs/tags/2.0.1.tar.gz"
-  sha256 "2168df2d4d4ba04554d0e340d2be47f66f152fe1cc5770e9e14bd1bd735654b9"
+  url "https://github.com/nickgerace/gfold/archive/refs/tags/2.0.2.tar.gz"
+  sha256 "f3f3d7e0985c99cbddcf87145fa2378d76239c72fbc203881e3e95996c46e46c"
   license "Apache-2.0"
 
   depends_on "rust" => :build
