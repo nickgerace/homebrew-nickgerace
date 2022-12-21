bcfe905475b29c4222202388b7b4b605ef563017
Nick Gerace <nickagerace@gmail.com>
2022-12-21 16:47:16 -0500

    Update gfold to 4.2.0
    Signed-off-by: Nick Gerace <nickagerace@gmail.com>

--- Formula/gfold.rb
+++ Formula/gfold.rb
@@ -1,8 +1,8 @@
 class Gfold < Formula
   desc "CLI tool to help keep track of your Git repositories, written in Rust"
   homepage "https://nickgerace.dev"
-  url "https://github.com/nickgerace/gfold/archive/refs/tags/4.1.2.tar.gz"
-  sha256 "e1ff92ef3fef35be416bfbef9c7a198ffdb675499a4dde2c497e919b99201de9"
+  url "https://github.com/nickgerace/gfold/archive/refs/tags/4.2.0.tar.gz"
+  sha256 "1919135665bcab3144f0ca3987f4d0526e383d3d06fb33764f0e3215d3599cb4"
   license "Apache-2.0"
 
   depends_on "rust" => :build
